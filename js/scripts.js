93b17cb7d91ba0b747bc241221102058220d946a
Dale <student@epicodus.com>
2015-02-13 14:56:43 -0800

    correct input the users dimensions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
   var triangle = function(side1, side2, side3) {
     // Evaluates if the user's input is truly a triangle according to maths
     if (Math.pow(side1, 2) + Math.pow(side2, 2) > Math.pow(side3, 2) || Math.pow(side1, 2) + Math.pow(side3, 2) <= Math.pow(side2, 2) || Math.pow(side2, 2) + Math.pow(side3, 2) <= Math.pow(side1, 2)) {
-      // Evaluates if the given triangle is an equilateral triangle
+      // Evaluates if the given sides are an equilateral triangle.
       if (side1 === side2 && side2 === side3 && side1 === side3) {
-        return true;
-        // Evaluates if
+        return "That's an equilateral triangle!";
+        // Evaluates if the given sides are an isosceles triangle.
       } else if (side1 === side2 || side1 == side3 || side2 === side3) {
-        return true;
+        return "That's an isosceles triangle!";
+        // Evalutates if the given sides are an scalene triangle.
       } else if (side1 !== side2 && side2 !== side3 && side1 !== side3) {
-        return true;
+        return "That's a scalene triangle!";
       };
     } else {
       return "That is not a triangle, sorry";
@@ -20,16 +21,10 @@ $(document).ready(function() {
   $("form#triangle").submit(function(event) {
     var side1 = parseFloat($("input#side1").val());
     var side2 = parseFloat($("input#side2").val());
-    var side3 = parseFloat($("intput#side3").val());
+    var side3 = parseFloat($("input#side3").val());
     var result = triangle(side1, side2, side3);
 
-    $(".triangle").text(result);
-    if (result) {
-      $(".triangle").text("That is a triangle!");
-    } else if (!results) {
-      $(".not").text("That is not a triangle, sorry");
-    }
-
+    $(".triangleResult").text(result);
     $("#result").show();
     event.preventDefault();
   });
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,14 @@
 describe('triangle', function(){
   it("is true if all three inputs are equal", function(){
-    expect(triangle(2, 2, 2)).to.equal(true);
+    expect(triangle(2, 2, 2)).to.equal("That's an equilateral triangle!");
   });
 
   it("is true if only inputs are equal", function(){
-    expect(triangle(2,2,1)).to.equal(true);
+    expect(triangle(2,2,1)).to.equal("That's an isosceles triangle!");
   });
 
   it("is true if all sides are not equal", function(){
-    expect(triangle(3,2.5,2)).to.equal(true);
+    expect(triangle(3,2.5,2)).to.equal("That's a scalene triangle!");
   });
 
   it("is false if it's not a triangle", function(){
